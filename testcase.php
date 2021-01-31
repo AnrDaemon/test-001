f9c2bc8d7d048e20cc4db5f7d8cf6c657313bdb7
AnrDaemon <anrdaemon@yandex.ru>
2021-01-31 19:27:30 +0300

    Reproduction case for smarty-php/smarty#639

--- testcase.php
+++ testcase.php
@@ -7,3 +7,5 @@ $smarty->display(__FILE__);
 __HALT_COMPILER();
 ?>PHP: {$smarty.const.PHP_VERSION}
 Smarty: {$smarty.version}
+{include "templates/insert-VkVerify.smarty"}
+{include "templates/insert-GTags.smarty" inline}
